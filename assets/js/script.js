74e465ac21dec9102c1dd5201ab8f93786dafd37
Farruh Zoirov <fzoirov29@gmail.com>
2024-01-08 21:42:46 +0500

    project is in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,9 +49,13 @@ const tickerTrack = document.querySelector('.ticker__track');
 for (let i = 0; i < 50; i++) {
     tickerTrack.innerHTML += `
              <span class="ticker__item">Web dizayn</span>
+             <img src="${trickerArrow[0]}" alt="">
              <span class="ticker__item">Packaging dizayn</span>
+             <img src="${trickerArrow[0]}" alt="">
              <span class="ticker__item">Brand dizayn</span>
+             <img src="${trickerArrow[0]}" alt="">
              <span class="ticker__item">Grafik dizayn</span>
+             <img src="${trickerArrow[0]}" alt="">
        `
 }
 
